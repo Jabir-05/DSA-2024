c0234ffffb95a474d11fcf1e1a1dae55065cb946
JABIR IMTEYAZ <knock5jabir@gmail.com>
2024-06-22 14:23:15 +0530

    Revision2

---
+++ Jabir's code/Revision2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+ using namespace std;
+  int  main (){
+
+
+   // Q:- Write a program to print + pattern given below ?
+//   *
+//   *
+// *****
+//   *
+//   *
+ 
+
+  int n =5;
+ 
+  for ( int line =0; line <n; line++){
+    for(int i=0; i<n;i++){
+        if(i==n/2)
+            cout<<"*";
+
+        else if 
+        (line==n/2)
+        cout<<"*";
+       
+        else
+            cout<<" ";
+        }
+        cout<<endl;
+}    
+
+   
+  return 0;
+  }
