f740238f017647c08a7d303f530cd6deb55f1471
JABIR IMTEYAZ <knock5jabir@gmail.com>
2024-07-13 16:07:02 +0530

    Arrays

---
+++ Jabir's code/Arrays.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Arrays are used to store homogeneous items
+    // Arrays are contiguous memory
+    // index = location of any element
+
+    // One dimensional array
+   // int arr[] = {1, 3, 2, 5, 6};
+
+    // cout << sizeof(arr) << endl;
+    // cout<<sizeof(arr)/sizeof(arr[0])<<endl;  // length
+   // int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Traversing of an array
+    // For  loop
+    //  for(int i=0; i<size;i++){
+    //     cout<<arr[i]<<endl;
+    //  }
+
+    //      // For each loop
+    //       for(int ele:arr){
+    //         cout<<ele<<endl;
+    //       }
+
+    //        // while loop
+    //         int index= 0;
+    //         while(index<size){
+    //             cout<<arr[index]<<endl;
+    //             index++;
+    //         }
+
+    //   input (using for loop)
+       // v[5];
+    //    for(int i=0; i<5;i++){
+    //     cin>>v[i];
+
+       //}
+
+       // fot each loop
+
+    //    for(char &element:v){
+    //     cin>>element;
+    //    }
+    //    for(int i=0; i<5;i++){
+    //     cout<<v[i]<<endl;
+
+    //    }
+ 
+
+   // Q:- calculate the sum of all the element  is the given array?
+
+    //  int arr[]= {1,3,4,5,6,7,8};
+    //  int size =sizeof(arr)/sizeof(arr[0]);
+
+    //  int sum=0;
+    //  for(int i=0;i<size;i++){
+    //     sum+=arr[i];
+
+    //  }
+    //  cout<<sum<<endl;
+
+        // Q:- find the maximum value out of all the element in the array?
+// int arr[]={1,3,18,7,9};
+// int max=arr[0];
+
+//            for(int i=0; i<5;i++){
+//     if(arr[i]>max){
+//         max=arr[i];
+//     }
+//            }
+//            cout<<max<<endl;
+   
+
+   
+    return 0;
+}
--- Jabir's code/tempCodeRunnerFile.cpp
+++ Jabir's code/tempCodeRunnerFile.cpp
@@ -1,12 +1,3 @@
-#include<iostream>
- using namespace std;
-  int main (){     
-    //  Q1:- Rectangular Parttern 
-    for(int i=1; i<-3; i++){
-     for(int j=1;j<=6; j++){
-      cout<<"*";
-     }
-     cout<<endl;
-    }
- return 0;
-  }
+for(int i=0; i<size;i++){
+//     cout<<arr[i]<<endl;
+//  }
